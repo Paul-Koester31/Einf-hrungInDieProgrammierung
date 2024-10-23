3d2b2d8aaea5ea35168fc8420d5dfd0f3016bbe2
PaulK <ia21.koester.paul@bk-hennef.de>
2024-10-23 09:25:53 +0200

    start

--- uebung03/src/BitExtraktion.java
+++ uebung03/src/BitExtraktion.java
@@ -1,13 +1,8 @@
-import javax.sound.midi.Soundbank;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class BitExtraktion {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int x;
         int y;
         int a;
-
 
         try {
             x = Integer.parseInt(args[0]);
@@ -15,11 +10,11 @@ public class BitExtraktion {
             a = Integer.parseInt(args[2]);
 
             if ((x >= 0 && x <= 31) && (y >= 0 & y <= 31) && (x != y) && (a >= 0 && a <= Math.pow(2, 31))) {
-                 String bin = Integer.toBinaryString(a);
+                int ergebnis = ((a >> x) & 1) + ((a >> y) & 1);
+                System.out.println(ergebnis);
 
-                System.out.println(Integer.toBinaryString(a));
-            } else throw new InputMismatchException();
-        } catch (InputMismatchException e) {
+            } else throw new Exception();
+        } catch (Exception e) {
             System.out.println("Falsche Eingabe!!");
         }
 
