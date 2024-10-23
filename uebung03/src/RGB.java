6bc6341587d1912681cf33ff68afe82396f17bc3
PaulK <ia21.koester.paul@bk-hennef.de>
2024-10-23 22:23:52 +0200

    start

---
+++ uebung03/src/RGB.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class RGB {
+
+    byte g;
+    byte b;
+    //int rgb = (r>>g>>b);
+
+    public static void main(String[] args) {
+        byte r= 55;
+        System.out.println(r);
+    }
+}
