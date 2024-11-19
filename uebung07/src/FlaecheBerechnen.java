6f9b332af14e7d3362dfcf66219be3f4a750a4ad
PaulK <ia21a.koester.paul@bk-hennef.de>
2024-11-19 09:40:14 +0100

    start

--- uebung07/src/FlaecheBerechnen.java
+++ uebung07/src/FlaecheBerechnen.java
@@ -1,10 +1,10 @@
 public class FlaecheBerechnen {
     public static void main(String[] args) {
         float[][] pol = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
-        System.out.println(fleacheBerechnen(pol));
+        System.out.println(flaecheBerechnen(pol));
     }
 
-    public static float fleacheBerechnen(float[][] coord) {
+    public static float flaecheBerechnen(float[][] coord) {
         float a = 0;
         int n = coord.length;
         for (int i = 0; i < n; i++) {
